3d14dfb316ee2d6b128661fac83c08041afa0237
Yutaka Kato <mikan@aomikan.org>
2026-08-01 02:58:52 +0900

    update html standard

--- cgi-bin/mdevents.py
+++ cgi-bin/mdevents.py
@@ -107,9 +107,8 @@ def print_event_list(file_list, limit):
 
 html_header = """Content-Type: text/html
 
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+<!DOCTYPE html>
+<html lang="ja">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Camera %d: MOTION</title>
